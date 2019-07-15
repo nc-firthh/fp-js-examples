1a1856cc493588368ce3bf7b9206c4ed9b08a61c
Henry Firth <firthh@firthh-00185.blc-hq.com>
2019-07-15 17:35:57 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const { forEach, map, multiply, pipe } = require('lodash/fp');
+
+// Impure example
+const impure = (user) => {
+  function getOlder() {
+    user.age += 1;
+    return user;
+  }
+
+  getOlder();
+
+  console.log(user.age);
+  return user;
+};
+
+// Pure example
+const pure = (user) => {
+  function getOlder(user) {
+    const age = user.age + 1;
+    return Object.assign({}, user, { age });
+  }
+
+  const olderUser = getOlder(user);
+
+  console.log(olderUser.age);
+  return olderUser;
+};
+
+// Imperative example
+const imperative = (array) => {
+  var newArray = [];
+  var i;
+
+  for (i = 0; i < array.length; i++) {
+    var doubledValue = array[i] * 2;
+    console.log(doubledValue);
+    newArray.push(doubledValue);
+  }
+
+  return newArray;
+};
+
+// Declarative example
+const declarative = (array) => {
+  const double = multiply(2);
+  const doubleArray = map(double);
+  const logEach = forEach(console.log);
+
+  const doubleAndLog = pipe(
+    doubleArray,
+    logEach,
+  );
+
+  return doubleAndLog(array);
+};
+
+
+impure({ age: 20 });
+pure({ age: 20 });
+imperative([1, 2, 3]);
+declarative([1, 2, 3]);
